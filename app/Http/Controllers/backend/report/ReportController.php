c9f0af49e2d7778216f97e8aba8e5cf4db481e18
Michael <mikimamo921@gmail.com>
2023-05-16 15:03:32 +0300

    change in report date

--- app/Http/Controllers/backend/report/ReportController.php
+++ app/Http/Controllers/backend/report/ReportController.php
@@ -52,14 +52,14 @@ class ReportController extends Controller
                             ->orderBy('sum', 'DESC')
                             ->get('savingtypeid', 'sum');
             $data['topLiability'] = MyLiability::leftJoin('my_liability_payments', 'my_liability_payments.liabilityid','=','my_liabilities.id')->where('userid',$userId)
-                            ->whereBetween('my_liability_payments.date', [$fromDate, $toDate])
+                            ->whereBetween('my_liabilities.date', [$fromDate, $toDate])
                             ->groupBy('lender')
                             ->selectRaw('sum(my_liabilities.amount) as total, sum(my_liability_payments.amount) as payed, sum(my_liabilities.amount)-sum(my_liability_payments.amount) as unpayed, lender')
                             ->orderBy('unpayed', 'DESC')
                             ->get('lender', 'total', 'payed', 'unpayed');
 
             $data['topReceivable'] = MyReceivable::leftJoin('my_receivable_payments', 'my_receivable_payments.receivableid','=','my_receivables.id')->where('userid', $userId)
-                            ->whereBetween('my_receivable_payments.date', [$fromDate, $toDate])
+                            ->whereBetween('my_receivables.date', [$fromDate, $toDate])
                             ->groupBy('borrower')
                             ->selectRaw('sum(my_receivables.amount) as total, sum(my_receivable_payments.amount) as payed, sum(my_receivables.amount)-sum(my_receivable_payments.amount) as unpayed, borrower')
                             ->orderBy('unpayed', 'DESC')
