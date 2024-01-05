7f620e67864e67a93dfb95a4ad96ab1fe4d98299
Michael <mikimamo921@gmail.com>
2024-01-05 16:50:11 +0300

    pdf for two companies

--- resources/views/admin/pdf/viewCustomers.blade.php
+++ resources/views/admin/pdf/viewCustomers.blade.php
@@ -72,21 +72,9 @@ use Carbon\Carbon;
                                                         BA</span>
                                                     </button>
                                                     <button type="button" class="btn btn-sm btn-secondary float-left"
-                                                        data-toggle="modal" data-target="#viewBa{{$customer->id}}">
-                                                        BA</span>
-                                                    </button>
-                                                    <button type="button" class="btn btn-sm btn-secondary float-left"
-                                                        data-toggle="modal" data-target="#viewBa{{$customer->id}}">
-                                                        BA</span>
-                                                    </button>
-                                                    <button type="button" class="btn btn-sm btn-secondary float-left"
-                                                        data-toggle="modal" data-target="#viewBa{{$customer->id}}">
-                                                        BA</span>
-                                                    </button>
-                                                    <!-- <button type="button" class="btn btn-sm btn-secondary float-left"
                                                         data-toggle="modal" data-target="#viewDu{{$customer->id}}">
                                                         DU</span>
-                                                    </button> -->
+                                                    </button>
                                                 </div>
                                             </td>
                                         </tr>
@@ -114,9 +102,9 @@ use Carbon\Carbon;
     </div>
     @foreach($allData as $key=>$customer)
     <div class="modal fade text-left" id="viewBa{{$customer->id}}">
-        <div class="modal-dialog modal-lg" id="downloadBa{{$customer->id}}">
-            <div class="modal-content" id="printBa{{$customer->id}}">
-                <div class="modal-body" id="downloadKendo{{$customer->id}}">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-body" id="downloadBa{{$customer->id}}">
                     <img src="{{url('uploads/pdfimages/static/ba.png')}}" style="height:60px; width:100%;">
                     <div class="row">
                         <div class="col-3 text-center">
@@ -332,21 +320,233 @@ use Carbon\Carbon;
                 </div>
                 <div class="modal-footer text-center">
                     <!-- <button id="btnDownloadBaModal{{$customer->id}}" class="btn btn-secondary">Download</button> -->
-                    <button onclick='saveToPDF{{$customer->id}}()' class="btn btn-success">Save to PDF</button>
+                    <button onclick='saveToPDFBa{{$customer->id}}()' class="btn btn-success">Save to PDF</button>
                 </div>
             </div>
             <!-- /.modal-content -->
         </div>
         <!-- /.modal-dialog -->
     </div>
-    <div class=" modal fade text-left" id="viewDu{{$customer->id}}">
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content downloadDu{{$customer->id}}" id="printDu{{$customer->id}}">
-                <div class="modal-header">
-                    <img style="height:150px; width:100%;" src="{{url('uploads/pdfimages/static/du.png')}}">
+    <div class="modal fade text-left" id="viewDu{{$customer->id}}">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-body" id="downloadDu{{$customer->id}}">
+                    <img src="{{url('uploads/pdfimages/static/du.png')}}" style="height:60px; width:100%;">
+                    <div class="row">
+                        <div class="col-3 text-center">
+                            <img src="{{url('uploads/pdfimages/protraitimage/'.$customer->protraitimage)}}"
+                                style="height:120px; width:150px;" />
+                        </div>
+                        <div class="col-9">
+                            <small class="float-left text-bold">PHONE:- {{$customer->phone}}</small>
+                            <small class="text-danger float-right">REF:</small>
+                            <table class="table table-bordered table-condensed table-condensed table-sm text-sm">
+                                <tr>
+                                    <th>POST APPLIED FOR</th>
+                                    <td>{{$customer->appliedfor}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>MONTHLY SALARY</th>
+                                    <td>{{$customer->salary}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>CONTRACT PERIOD</th>
+                                    <td>{{$customer->contactperiod}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <table class="table table-bordered table-condensed table-sm text-sm">
+                                <tr>
+                                    <th>NAME IN FULL</th>
+                                    <th>{{$customer->fullname}}</th>
+                                    <td>arabic part</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <small>DETAILS OF APPLICATION</small>
+                            <table class="table table-bordered table-condensed table-condensed table-sm text-sm">
+                                <tr>
+                                    <td><b>NATIONALITY</b></td>
+                                    <td>ETHIOPIA</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <td><b>RELEGION</b></td>
+                                    <td>{{$customer->relegion}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>DATE OF BIRTH</th>
+                                    <td>{{$customer->dateofbirth}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>AGE</th>
+                                    <td>{{ Carbon::parse($customer->dateofbirth)->age ?? 0 }}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>PLACE OF BIRTH</th>
+                                    <td>{{$customer->place}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>LIVING TOWN</th>
+                                    <td>{{$customer->town}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>MARITAL STATUS</th>
+                                    <td>{{$customer->maritalstatus}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>NO. OF CHILDEREN</th>
+                                    <td>{{$customer->children}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>WEIGHT</th>
+                                    <td>{{$customer->weight}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>HEIGHT</th>
+                                    <td>{{$customer->height}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>COMPLEXION</th>
+                                    <td>{{$customer->complexion}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>EDUCATIONAL</th>
+                                    <td>{{$customer->education}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                            </table>
+                            <small class="float-left">KNOWLEDGE OF LANGUAGE</small>
+                            <small class="float-right">arabic part</small>
+                            <table class="table table-bordered table-condensed table-sm text-sm">
+                                <tr>
+                                    <th>LANGUAGE</th>
+                                    <th>SPEAK</th>
+                                    <th>WRITE</th>
+                                    <th>READ</th>
+                                </tr>
+                                <tr>
+                                    <th>ARABIC</th>
+                                    <td>{{$customer->arabics == 1?'YES':'NO'}}</td>
+                                    <td>{{$customer->arabicw == 1?'YES':'NO'}}</td>
+                                    <td>{{$customer->arabicr == 1?'YES':'NO'}}</td>
+                                </tr>
+                                <tr>
+                                    <th>ENGLISH</th>
+                                    <td>{{$customer->englishs == 1?'YES':'NO'}}</td>
+                                    <td>{{$customer->englishw == 1?'YES':'NO'}}</td>
+                                    <td>{{$customer->englishr == 1?'YES':'NO'}}</td>
+                                </tr>
+                            </table>
+                            <small class="float-left">PREVIOUS EMPLOYEMENT BROAD</small>
+                            <small class="float-right">arabic part</small>
+                            <table class="table table-condensed table-bordered table-sm text-sm">
+                                <tr>
+                                    <th>COUNTRY</th>
+                                    <td>{{$customer->empcountry}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>PERIOD</th>
+                                    <td>{{$customer->empperiod}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                            </table>
+                            <small class="float-left">WORK EXPERIANCE</small>
+                            <small class="float-right">arabic part</small>
+                            <table class="table table-condensed table-bordered table-sm text-sm">
+                                <tr>
+                                    <th>DRIVING</th>
+                                    <td>{{$customer->driving == 1?'YES':'NO'}}</td>
+                                    <td>arabicpart</td>
+                                </tr>
+                                <tr>
+                                    <th>COOKING</th>
+                                    <td>{{$customer->cooking == 1?'YES':'NO'}}</td>
+                                    <td>arabicpart</td>
+                                </tr>
+                                <tr>
+                                    <th>CLEANING</th>
+                                    <td>{{$customer->cleaning == 1?'YES':'NO'}}</td>
+                                    <td>arabicpart</td>
+                                </tr>
+                                <tr>
+                                    <th>WASHING</th>
+                                    <td>{{$customer->washing == 1?'YES':'NO'}}</td>
+                                    <td>arabicpart</td>
+                                </tr>
+                                <tr>
+                                    <th>BABY SITTING</th>
+                                    <td>{{$customer->babyseat == 1?'YES':'NO'}}</td>
+                                    <td>arabicpart</td>
+                                </tr>
+                                <tr>
+                                    <th>SEWING</th>
+                                    <td>{{$customer->sewing == 1?'YES':'NO'}}</td>
+                                    <td>arabicpart</td>
+                                </tr>
+                            </table>
+                            <small>{{$customer->delala}}</small>
+                            <br>
+                            <small class="float-left">DATE _____________________________________ </small>
+                            <small class="float-right">arabic part</small>
+                        </div>
+                        <div class="col-6">
+                            <small class="float-left">PASSPORT DETAILS</small>
+                            <table class="table table-bordered table-condensed table-sm text-sm">
+                                <tr>
+                                    <th>NUMBER</th>
+                                    <td>{{$customer->passportnumber}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>DATE OF ISSUE</th>
+                                    <td>{{$customer->pdate}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>PLACE OF ISSUE</th>
+                                    <td>{{$customer->pplace}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                                <tr>
+                                    <th>DATE OF ESP.</th>
+                                    <td>{{$customer->pexpiry}}</td>
+                                    <td>arabic part</td>
+                                </tr>
+                            </table>
+                            <img src="{{url('uploads/pdfimages/fullimage/'.$customer->fullimage)}}"
+                                style="height:700px; width:100%; border: 1px;" alt="">
+                        </div>
+                    </div>
+                    <img src="{{url('uploads/pdfimages/static/duFooter.png')}}" style="height:30px; width:100%;">
+                    <div class="row mt-5">
+                        <img src="{{url('uploads/pdfimages/passportimage/'.$customer->passportimage)}}"
+                            style="height:800px; width:100%;">
+                    </div>
                 </div>
-                <div class="modal-body">
-                    <div></div>
+                <div class="modal-footer text-center">
+                    <!-- <button id="btnDownloadBaModal{{$customer->id}}" class="btn btn-secondary">Download</button> -->
+                    <button onclick='saveToPDFDu{{$customer->id}}()' class="btn btn-success">Save to PDF</button>
                 </div>
             </div>
             <!-- /.modal-content -->
@@ -368,11 +568,19 @@ use Carbon\Carbon;
     </script>
     <script>
         @foreach($allData as $key=> $customer)
-        function saveToPDF{{$customer->id}}() {
+        function saveToPDFBa{{$customer->id}}() {
             window.scrollTo(0, 0);
-            var contentToSave = document.getElementById('downloadKendo{{$customer->id}}');
+            var contentToSave = document.getElementById('downloadBa{{$customer->id}}');
             html2pdf(contentToSave, {
                 filename: '{{$customer->fullname}} BA.pdf',
+                jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+            });
+        }
+        function saveToPDFDu{{$customer->id}}() {
+            window.scrollTo(0, 0);
+            var contentToSave = document.getElementById('downloadDu{{$customer->id}}');
+            html2pdf(contentToSave, {
+                filename: '{{$customer->fullname}} DU.pdf',
                 jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
             });
         }
